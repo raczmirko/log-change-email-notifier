26b01485e8525db31b8a7061794d51d64d36609c
raczmirko <m1rk0d3v@gmail.com>
2023-12-15 11:50:50 +0100

    Initial commit

---
+++ src/main/java/hu/okrim/TextFileChangeEmailSender/TextFileChangeEmailSenderApplication.java
@@ -0,0 +1,13 @@
+package hu.okrim.TextFileChangeEmailSender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextFileChangeEmailSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextFileChangeEmailSenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/okrim/TextFileChangeEmailSender/TextFileChangeEmailSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.okrim.TextFileChangeEmailSender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextFileChangeEmailSenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
