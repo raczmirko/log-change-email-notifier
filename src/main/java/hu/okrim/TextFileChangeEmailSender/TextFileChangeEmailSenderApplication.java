b2eee72c957cd0c850e08520c1ea0f25e3f2a3a8
raczmirko <m1rk0d3v@gmail.com>
2023-12-15 13:21:18 +0100

    feat: overridden the run method to use toEmail from the console (given when the application is called) to be more dynamic

--- src/main/java/hu/okrim/TextFileChangeEmailSender/TextFileChangeEmailSenderApplication.java
+++ src/main/java/hu/okrim/TextFileChangeEmailSender/TextFileChangeEmailSenderApplication.java
@@ -1,13 +1,25 @@
 package hu.okrim.TextFileChangeEmailSender;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TextFileChangeEmailSenderApplication {
+public class TextFileChangeEmailSenderApplication implements CommandLineRunner {
+	@Autowired
+	private EmailSenderService senderService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TextFileChangeEmailSenderApplication.class, args);
 	}
-
+@Override
+public void run(String... args) {
+	if (args.length == 0) {
+		System.out.println("Usage: java -jar your-jar-file.jar <toEmail>");
+	} else {
+		String toEmail = args[0];
+		senderService.sendMail(toEmail, "Test", "This is a test mail.");
+	}
+}
 }
