a7f2a8fb8564cbcabc453978251295f845602ade
raczmirko <m1rk0d3v@gmail.com>
2023-12-15 13:20:09 +0100

    feat: added EmailSenderService to send emails according to the call parameters and email address given in application.properties

---
+++ src/main/java/hu/okrim/TextFileChangeEmailSender/EmailSenderService.java
@@ -0,0 +1,30 @@
+package hu.okrim.TextFileChangeEmailSender;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSenderService {
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
+    public void sendMail(String toEmail,
+                         String subject,
+                         String body){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(fromEmail);
+        message.setTo(toEmail);
+        message.setSubject(subject);
+        message.setText(body);
+
+        mailSender.send(message);
+
+        System.out.println("Mail sent successfully...");
+    }
+}
